397ebe100c3309c7b6d822ec0e6256c939efe87f
Javaid  Mir <javaid.mir@enablenow.com>
2026-08-29 14:31:02 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
+  output: "export",
 };
 
 export default nextConfig;
